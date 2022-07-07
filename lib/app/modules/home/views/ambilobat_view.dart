c171d7a9d8be9525d464d8b8947f94b1e5e9449d
msyarifuddin <muh.syarifuddin@gmail.com>
2022-07-07 12:59:22 +0700

    landscape kamera depan

--- lib/app/modules/home/views/ambilobat_view.dart
+++ lib/app/modules/home/views/ambilobat_view.dart
@@ -14,7 +14,7 @@ class AmbilobatView extends GetView {
   MobileScannerController controllerMSC = MobileScannerController(
     torchEnabled: false,
     // formats: [BarcodeFormat.qrCode]
-    // facing: CameraFacing.front,
+    facing: CameraFacing.front,
   );
 
   Widget build(BuildContext context) {
@@ -57,51 +57,62 @@ class AmbilobatView extends GetView {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      IconButton(
-                        color: Colors.white,
-                        icon: ValueListenableBuilder(
-                          valueListenable: controllerMSC.torchState,
-                          builder: (context, state, child) {
-                            if (state == null) {
-                              return const Icon(
-                                Icons.flash_off,
-                                color: Colors.grey,
-                              );
-                            }
-                            switch (state as TorchState) {
-                              case TorchState.off:
-                                return const Icon(
-                                  Icons.flash_off,
-                                  color: Colors.grey,
-                                );
-                              case TorchState.on:
-                                return const Icon(
-                                  Icons.flash_on,
-                                  color: Colors.yellow,
-                                );
-                            }
-                          },
-                        ),
-                        iconSize: 32.0,
-                        onPressed: () => controllerMSC.toggleTorch(),
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        icon: homeCtrl.isStarted.value
-                            ? const Icon(Icons.stop)
-                            : const Icon(Icons.play_arrow),
-                        iconSize: 32.0,
-                        onPressed: () {
-                          homeCtrl.isStarted.value
-                              ? controllerMSC.stop()
-                              : controllerMSC.start();
-                          homeCtrl.isStarted.value = !homeCtrl.isStarted.value;
-                        },
-                        // setState(() {
-                        //   isStarted ? controller.stop() : controller.start();
-                        //   isStarted = !isStarted;
-                        // }),
-                      ),
+                      // Row(
+                      //   children: [
+                      //     IconButton(
+                      //       color: Colors.white,
+                      //       icon: ValueListenableBuilder(
+                      //         valueListenable: controllerMSC.torchState,
+                      //         builder: (context, state, child) {
+                      //           if (state == null) {
+                      //             return const Icon(
+                      //               Icons.flash_off,
+                      //               color: Colors.grey,
+                      //             );
+                      //           }
+                      //           switch (state as TorchState) {
+                      //             case TorchState.off:
+                      //               return const Icon(
+                      //                 Icons.flash_off,
+                      //                 color: Colors.grey,
+                      //               );
+                      //             case TorchState.on:
+                      //               return const Icon(
+                      //                 Icons.flash_on,
+                      //                 color: Colors.yellow,
+                      //               );
+                      //           }
+                      //         },
+                      //       ),
+                      //       iconSize: 32.0,
+                      //       onPressed: () => controllerMSC.toggleTorch(),
+                      //     ),
+                      //     Text("Flash")
+                      //   ],
+                      // ),
+                      // Row(
+                      //   children: [
+                      //     IconButton(
+                      //       color: Colors.white,
+                      //       icon: homeCtrl.isStarted.value
+                      //           ? const Icon(Icons.stop)
+                      //           : const Icon(Icons.play_arrow),
+                      //       iconSize: 32.0,
+                      //       onPressed: () {
+                      //         homeCtrl.isStarted.value
+                      //             ? controllerMSC.stop()
+                      //             : controllerMSC.start();
+                      //         homeCtrl.isStarted.value =
+                      //             !homeCtrl.isStarted.value;
+                      //       },
+                      //       // setState(() {
+                      //       //   isStarted ? controller.stop() : controller.start();
+                      //       //   isStarted = !isStarted;
+                      //       // }),
+                      //     ),
+                      //     Text("Pause")
+                      //   ],
+                      // ),
                       Obx(() => Center(
                             child: SizedBox(
                               width: MediaQuery.of(context).size.width - 200,
@@ -118,56 +129,67 @@ class AmbilobatView extends GetView {
                               ),
                             ),
                           )),
-                      IconButton(
-                        color: Colors.white,
-                        icon: ValueListenableBuilder(
-                          valueListenable: controllerMSC.cameraFacingState,
-                          builder: (context, state, child) {
-                            if (state == null) {
-                              return const Icon(Icons.camera_front);
-                            }
-                            switch (state as CameraFacing) {
-                              case CameraFacing.front:
-                                return const Icon(Icons.camera_front);
-                              case CameraFacing.back:
-                                return const Icon(Icons.camera_rear);
-                            }
-                          },
-                        ),
-                        iconSize: 32.0,
-                        onPressed: () => controllerMSC.switchCamera(),
+                      Row(
+                        children: [
+                          IconButton(
+                            color: Colors.white,
+                            icon: ValueListenableBuilder(
+                              valueListenable: controllerMSC.cameraFacingState,
+                              builder: (context, state, child) {
+                                if (state == null) {
+                                  return const Icon(Icons.camera_front);
+                                }
+                                switch (state as CameraFacing) {
+                                  case CameraFacing.front:
+                                    return const Icon(Icons.camera_front);
+                                  case CameraFacing.back:
+                                    return const Icon(Icons.camera_rear);
+                                }
+                              },
+                            ),
+                            iconSize: 32.0,
+                            onPressed: () => controllerMSC.switchCamera(),
+                          ),
+                          Text("Camera")
+                        ],
                       ),
-                      IconButton(
-                        color: Colors.white,
-                        icon: const Icon(Icons.image),
-                        iconSize: 32.0,
-                        onPressed: () async {
-                          final ImagePicker _picker = ImagePicker();
-                          // Pick an image
-                          final XFile? image = await _picker.pickImage(
-                            source: ImageSource.gallery,
-                          );
-                          if (image != null) {
-                            if (await controllerMSC.analyzeImage(image.path)) {
-                              // if (!mounted) return;
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Barcode found!'),
-                                  backgroundColor: Colors.green,
-                                ),
-                              );
-                            } else {
-                              // if (!mounted) return;
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('No barcode found!'),
-                                  backgroundColor: Colors.red,
-                                ),
-                              );
-                            }
-                          }
-                        },
-                      ),
+                      // Row(
+                      //   children: [
+                      //     IconButton(
+                      //       color: Colors.white,
+                      //       icon: const Icon(Icons.image),
+                      //       iconSize: 32.0,
+                      //       onPressed: () async {
+                      //         final ImagePicker _picker = ImagePicker();
+                      //         // Pick an image
+                      //         final XFile? image = await _picker.pickImage(
+                      //           source: ImageSource.gallery,
+                      //         );
+                      //         if (image != null) {
+                      //           if (await controllerMSC
+                      //               .analyzeImage(image.path)) {
+                      //             // if (!mounted) return;
+                      //             ScaffoldMessenger.of(context).showSnackBar(
+                      //               const SnackBar(
+                      //                 content: Text('Barcode found!'),
+                      //                 backgroundColor: Colors.green,
+                      //               ),
+                      //             );
+                      //           } else {
+                      //             // if (!mounted) return;
+                      //             ScaffoldMessenger.of(context).showSnackBar(
+                      //               const SnackBar(
+                      //                 content: Text('No barcode found!'),
+                      //                 backgroundColor: Colors.red,
+                      //               ),
+                      //             );
+                      //           }
+                      //         }
+                      //       },
+                      //     ),
+                      //     Text("Image")
+                      //   ],
+                      // ),
                     ],
                   ),
                 ),
